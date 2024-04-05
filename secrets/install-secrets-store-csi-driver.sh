f23f55e37378e283fd8d7d5b09b412285831ef85
hmoon630 <skyuecx0630@gmail.com>
2024-04-05 15:28:52 +0900

    set syncSecret.enabled=false by default

--- secrets/install-secrets-store-csi-driver.sh
+++ secrets/install-secrets-store-csi-driver.sh
@@ -3,8 +3,9 @@
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
 helm repo add aws-secrets-manager https://aws.github.io/secrets-store-csi-driver-provider-aws
 
+# Best practice is not to set syncSecret.enabled=true. If to do so, enable KMS encryption for cluster.
 helm upgrade --install -n kube-system csi-secrets-store \
-    --set syncSecret.enabled=true \
+    --set syncSecret.enabled=false \
     --set enableSecretRotation=true \
     --set rotationPollInterval=5s \
     secrets-store-csi-driver/secrets-store-csi-driver
