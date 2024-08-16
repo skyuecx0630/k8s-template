8211578c86cd4abaca1cd1f3aea2705bc77de5de
hmoon630 <skyuecx0630@gmail.com>
2024-08-16 14:15:22 +0900

    Update karpenter version

--- autoscaler/karpenter/install_karpenter.sh
+++ autoscaler/karpenter/install_karpenter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-KARPENTER_VERSION="v0.33.0"
+KARPENTER_VERSION="0.37.0"
 KARPENTER_NAMESPACE="karpenter"
-KARPENTER_IAM_ROLE_ARN="arn:aws:iam::856210586235:role/KarpenterControllerRole-skills-cluster"
+KARPENTER_IAM_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/KarpenterControllerRole-${CLUSTER_NAME}"
 
-helm logout public.ecr.aws
+helm registry logout public.ecr.aws
 
 helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" \
   --set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=${KARPENTER_IAM_ROLE_ARN}" \
